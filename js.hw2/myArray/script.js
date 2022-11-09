4767b02b9eba450cc96ddfc2f8284766201728f7
RomanMartynyuk <martin06121991@gmail.com>
2022-11-10 01:04:58 +0200

    check pls

--- js.hw2/myArray/script.js
+++ js.hw2/myArray/script.js
@@ -4,19 +4,33 @@ console.log(myArray);
 myArray.length;
 console.log(myArray.length);
 
-Math.min.apply(null, [41,33,22,33,32,36,36,17,6,27,1,3,1,27,14,24,28,19,17,2,15,42,5,25,49,44,49,37,29,7,37,4,0,2,0,12,15,11,34,7,41,28,7,20,16,10,28,21,35,30])
-console.log(Math.min.apply(null, [41,33,22,33,32,36,36,17,6,27,1,3,1,27,14,24,28,19,17,2,15,42,5,25,49,44,49,37,29,7,37,4,0,2,0,12,15,11,34,7,41,28,7,20,16,10,28,21,35,30]));
+let max = myArray[0];
+for (let i = 1; i < myArray.length; ++i) {
+  if (myArray[i] > max) {
+    max = myArray[i];
+  }
+}
 
-Math.max.apply(null, [41,33,22,33,32,36,36,17,6,27,1,3,1,27,14,24,28,19,17,2,15,42,5,25,49,44,49,37,29,7,37,4,0,2,0,12,15,11,34,7,41,28,7,20,16,10,28,21,35,30])
-console.log(Math.max.apply(null, [41,33,22,33,32,36,36,17,6,27,1,3,1,27,14,24,28,19,17,2,15,42,5,25,49,44,49,37,29,7,37,4,0,2,0,12,15,11,34,7,41,28,7,20,16,10,28,21,35,30]));
+console.log(max);
 
-myArray.push('49');
+let min = myArray[0];
+for (let i = 1; i < myArray.length; ++i) {
+  if (myArray[i] < min) {
+    min = myArray[i];
+  }
+}
+
+console.log(min);
+
+const maxValue = 49;
+const minValue = 0;
+
+myArray.push(maxValue);
 
 myArray.length;
 console.log(myArray);
 console.log(myArray.length);
 
-
-myArray.unshift('0');
+myArray.unshift(minValue);
 console.log(myArray);
 console.log(myArray.length);
