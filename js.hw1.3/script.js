22a8db84c5686818dc493d5911169758280420bb
RomanMartynyuk <martin06121991@gmail.com>
2022-11-14 22:57:10 +0200

    fs

--- js.hw1.3/script.js
+++ js.hw1.3/script.js
@@ -7,7 +7,7 @@ const imgs = [
   "https://geographydirections.files.wordpress.com/2020/10/iceberg-antarctica-polar-blue-53389.jpeg?w=1880&h=1200&crop=1"
 ];
 
-let currentIndex = 0;
+let curIndex = 0;
 
 function changeImg(index) {
   const mainImgEl = document.getElementById("main-img");
@@ -15,101 +15,19 @@ function changeImg(index) {
 }
 
 function goNext() {
-  if (currentIndex < imgs.length - 1) {
-    currentIndex = currentIndex + 1;
+  if (curIndex < imgs.length - 1) {
+    curIndex = curIndex + 1;
   } else {
-    currentIndex = 0;
+    curIndex = 0;
   }
-  changeImg(currentIndex);
+  changeImg(curIndex);
 }
 
 function goPrev() {
-  if (currentIndex > 0) {
-    currentIndex = currentIndex - 1;
+  if (curIndex > 0) {
+    curIndex = curIndex - 1;
   } else {
-    currentIndex = imgs.length - 1;
+    curIndex = imgs.length - 1;
   }
-  changeImg(currentIndex);
+  changeImg(curIndex);
 }
-function addDots() {
-  const dotsEl = document.getElementById("dot-list");
-  dotsEl.innerHTML = dotHTML;
-  let dotHTML = "";
-  for (let i = 0; 1 < imgs.length; i = i + 1) {
-    dotHTML =
-      "<button id='dot1' class='dot-btn active' onclick='changeImg(${i})'></botton>";
-    dotsEl.innerHTML += dotHTML;
-  }
-}
-// function addDots() {
-//   const dotsEl = document.getElementById("dot-list");
-//   dotsEl.innerHtml = dotHtml;
-//   let dotHtml = "";
-//   for (let i = 0; 1 < images.length; i = i + 1) {
-//     dotHtml =
-//       "<button id='dot1' class='dot-btn active' onclick='changeImg(${i})'></botton>";
-//     dotsEl.innerHTML += dotHTML;
-//   }
-// }
-// addDots();
-
-// function changeImg(imgName) {
-//   const mainImgE1 = document.getElementById("main-img");
-
-//   if (imgName === "Gate") {
-//     mainImgE1.src = img1;
-//   }
-
-//   if (imgName === "Mountain") {
-//     mainImgE1.src = img2;
-//   }
-
-//   if (imgName === "Penguin") {
-//     mainImgE1.src = img3;
-//   }
-
-//   currentImgName = imgName;
-//   changeActiveClass();
-// }
-
-// function goNext() {
-//   if (currentImgName === "Gate") {
-//     changeImg("Mountain");
-//   } else if (currentImgName === "Mountain") {
-//     changeImg("Penguin");
-//   } else if (currentImgName === "Penguin") {
-//     changeImg("Gate");
-//   }
-// }
-
-// function goPrev() {
-//   if (currentImgName === "Gate") {
-//     changeImg("Penguin");
-//   } else if (currentImgName === "Penguin") {
-//     changeImg("Mountain");
-//   } else if (currentImgName === "Mountain") {
-//     changeImg("Gate");
-//   }
-// }
-
-// function changeActiveClass() {
-//   const dotEl1 = document.getElementById("dot1");
-//   const dotEl2 = document.getElementById("dot2");
-//   const dotEl3 = document.getElementById("dot3");
-
-//   dotEl1.classList.remove("active");
-//   dotEl2.classList.remove("active");
-//   dotEl3.classList.remove("active");
-
-//   if (currentImgName === "Gate") {
-//     dotEl1.classList.add("active");
-//   }
-
-//   if (currentImgName === "Mountain") {
-//     dotEl2.classList.add('active');
-//   }
-
-//   if (currentImgName === "Penguin") {
-//     dotEl3.classList.add('active');
-//   }
-// }
