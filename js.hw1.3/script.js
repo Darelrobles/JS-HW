da28729f9eff522d7dd7a2866df15c4d75dc10d1
RomanMartynyuk <martin06121991@gmail.com>
2022-11-14 22:41:52 +0200

    fs

--- js.hw1.3/script.js
+++ js.hw1.3/script.js
@@ -1,18 +1,21 @@
 const imgs = [
   "https://fbc.ua/wp-content/uploads/2021/07/ledenaya_antarktida.jpg",
   "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/Fryxellsee_Opt.jpg/1280px-Fryxellsee_Opt.jpg",
-  "https://publish.com.ua/images/2020/11/292063_odinokiy-pingvin_large.jpg",
+  "https://rare-gallery.com/uploads/posts/1152341-birds-penguins-animals-snow-winter-ice-Arctic-Penguin-bird-fauna-flightless-bird-vertebrate.jpg",
+  "https://i.natgeofe.com/n/c4f2589c-883d-40a4-bf64-239ac0ccc210/01-polar-animals-nationalgeographic_1528122_16x9.jpg?w=1200",
+  "https://i.natgeofe.com/n/d863d4f1-5e8d-4a8c-84c1-ab443c475a19/01_arctic_ocean_kxwd6b_16x9.jpg?w=1200",
+  "https://geographydirections.files.wordpress.com/2020/10/iceberg-antarctica-polar-blue-53389.jpeg?w=1880&h=1200&crop=1"
 ];
 
 let currentIndex = 0;
 
 function changeImg(index) {
   const mainImgEl = document.getElementById("main-img");
-  mainImgEl.src = images[index];
+  mainImgEl.src = imgs[index];
 }
 
 function goNext() {
-  if (currentIndex < images.length [- 1]) {
+  if (currentIndex < imgs.length - 1) {
     currentIndex = currentIndex + 1;
   } else {
     currentIndex = 0;
@@ -24,9 +27,19 @@ function goPrev() {
   if (currentIndex > 0) {
     currentIndex = currentIndex - 1;
   } else {
-    currentIndex = images.length - 1;
+    currentIndex = imgs.length - 1;
   }
   changeImg(currentIndex);
+}
+function addDots() {
+  const dotsEl = document.getElementById("dot-list");
+  dotsEl.innerHTML = dotHTML;
+  let dotHTML = "";
+  for (let i = 0; 1 < imgs.length; i = i + 1) {
+    dotHTML =
+      "<button id='dot1' class='dot-btn active' onclick='changeImg(${i})'></botton>";
+    dotsEl.innerHTML += dotHTML;
+  }
 }
 // function addDots() {
 //   const dotsEl = document.getElementById("dot-list");
