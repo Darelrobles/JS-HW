805fccbd4972e22d01039df38ed3bf2e8d01438d
RomanMartynyuk <martin06121991@gmail.com>
2022-11-22 10:44:22 +0200

    fs

--- js.func/script.js
+++ js.func/script.js
@@ -83,24 +83,24 @@ const height = 190;
 function whoAreYou(age, weight, height) {
   let res;
 
-  if (age < 1) {
+  if (!Number.isInteger(age)) {
+    res = "it is not a number";
+  } else if (age < 1) {
     res = "wrong symbol";
-  } else if (typeof age === 'string') {
-    res = "use numbers";
   } else if (age < 25) {
     res = "You are too young";
   } else if (age > 25) {
     res = "You are too old";
-  }  else if (typeof weight === 'string') {
-    res = "use numbers";
+  }  else if (!Number.isInteger(weight)) {
+    res = "it is not a number";
   }else if (weight < 1) {
     res = "wrong symbol";
   } else if (weight < 90) {
     res = "You are too slim";
   } else if (weight > 90) {
     res = "You are too fat";
-  } else if (typeof height === 'string') {
-    res = "use numbers";
+  } else if (!Number.isInteger(height)) {
+    res = "it is not a number";
   } else if (height < 1) {
     res = "wrong symbol";
   } else if (height < 190) {
