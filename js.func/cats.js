2c5189883aaebebd74e679f64373e8f172461060
RomanMartynyuk <martin06121991@gmail.com>
2022-11-28 15:17:10 +0200

    fs

--- js.func/cats.js
+++ js.func/cats.js
@@ -1,5 +1,37 @@
-let double = function(number){
-  return number * 2;
+// let double = function(number){
+//   return number * 2;
+// };
+
+// console.log(double(5)+double(7));
+
+// let randomNames = ["Paul", "Anna", "Otto", "Vivien", "Lisa"];
+// let randomBodyParts = ["Face", "Nose", "Hair"];
+// let randomWords = ["Alien", "Killer", "Stupid", "Apple", "Juice"];
+// console.log("Your " + pickRandomWord(randomBodyParts) + " is like a " + pickRandomWord(randomNames) +"'s " + pickRandomWord(randomWords) + "!!!");
+
+let pickRandomWord = function (words) {
+  return words[Math.floor(Math.random() * words.length)];
 };
 
-double(7);
+generateRandomInsult = function () {
+  let randomNames = ["Paul", "Anna", "Otto", "Vivien", "Lisa"];
+  let randomBodyParts = ["Face", "Nose", "Hair"];
+  let randomWords = ["Alien", "Killer", "Stupid", "Apple", "Juice"];
+
+  let randomString =
+    "Your " +
+    pickRandomWord(randomBodyParts) +
+    " is like a " +
+    pickRandomWord(randomNames) +
+    "'s " +
+    pickRandomWord(randomWords) +
+    "!!!";
+  return randomString;
+};
+
+console.log(generateRandomInsult());
+console.log(generateRandomInsult());
+console.log(generateRandomInsult());
+console.log(generateRandomInsult());
+console.log(generateRandomInsult());
+
